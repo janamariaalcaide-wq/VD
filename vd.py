3a53545d29df0bcd54a6f17793d126faebd0a4ac
Jana <janamaria.alcaide@gmail.com>
2025-08-30 20:43:44 +0200

    segnudo

--- vd.py
+++ vd.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/163osb8WRzi8viOgPJxv5283PmBntEPv9
 """
 
-!pip install streamlit
+
 
 import streamlit as st
 import pandas as pd
