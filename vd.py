7626fe532dc0c4adfcf7fe95408ec439c14dc960
Jana <janamaria.alcaide@gmail.com>
2025-08-31 09:35:36 +0200

    segnudo

--- vd.py
+++ vd.py
@@ -277,11 +277,11 @@ grouped_df = filtered_df.groupby('Model').agg({
 
 # --- Crear la visualización ---
 chart = alt.Chart(grouped_df).mark_circle().encode(
-    x=alt.X('Precision_macro', title='Precisión', scale=alt.Scale(domain=[0.7, 1])),
-    y=alt.Y('Recall_macro', title='Recall', scale=alt.Scale(domain=[0.7, 1])),
-    size=alt.Size('ROC_AUC', title='ROC_AUC', scale=alt.Scale(range=[200, 500])),
+    x=alt.X('Precision_macro', title='Precisión', scale=alt.Scale(domain=[0.7, 0.9])),
+    y=alt.Y('Recall_macro', title='Recall', scale=alt.Scale(domain=[0.7, 0.9])),
+    size=alt.Size('ROC_AUC', title='ROC_AUC', scale=alt.Scale(range=[300, 600])),
     color=alt.Color('Model', legend=alt.Legend(title="Model")),
-    tooltip=['Model']
+    tooltip=['Model','ROC_AUC','Precision_macro','Recall_macro']
 ).properties(
     width=700,
     height=500,
